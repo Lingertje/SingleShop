a0a0b11b9a0418f6ddf5199e7dfa45ab2d471107
Mart Linger <martlinger@gmail.com>
2023-06-01 09:44:39 +0200

    Add basic footer with link to github profile

---
+++ components/Footer.vue
@@ -0,0 +1,28 @@
+<script setup>
+
+</script>
+
+<template>
+	<footer>
+		<p><NuxtLink to="https://github.com/Lingertje">@Lingertje</NuxtLink> - {{ new Date().getFullYear() }}</p>
+	</footer>
+</template>
+
+<style module lang="scss">
+	footer {
+		margin-top: 2rem;
+		padding: 1rem 0;
+		background-color: var(--color-kux-background);
+
+		p {
+			margin: 0;
+			text-align: center;
+		}
+
+		a {
+			color: initial;
+			font-weight: 800;
+			text-decoration: none;
+		}
+	}
+</style>
--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,6 +1,8 @@
 <template>
 	<nav :class="$style['navbar']">
-		<img :class="$style['logo']" src="/kpn-logo.svg">
+		<NuxtLink to="/">
+			<img :class="$style['logo']" src="/kpn-logo.svg">
+		</NuxtLink>
 	</nav>
 </template>
 
@@ -10,13 +12,14 @@
 		top: 0;
 		padding: 1rem 0;
 		background-color: var(--color-kux-background);
-		box-shadow: 0 0 10px 15px rgba(0, 0, 0, 0.1);
+		box-shadow: 0 0 10px 2px rgba(0, 0, 0, 0.1);
 		z-index: 1;
 	}
 	.logo {
 		display: block;
 		height: 35px;
 		margin: 0 auto;
+		fill: var(--color-kux-text-primary);
 	}
 </style>
 
