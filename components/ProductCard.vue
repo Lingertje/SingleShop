485f99bf687cb08953f5f2399c44afbdbc570bca
Mart Linger <martlinger@gmail.com>
2023-06-01 09:29:33 +0200

    Display product cards in a grid with a placeholder image

---
+++ components/ProductCard.vue
@@ -0,0 +1,60 @@
+<script setup>
+	defineProps({
+		product: {
+			type: Object,
+			required: true
+		}
+	});
+</script>
+
+<template>
+	<div :class="$style['card']">
+		<NuxtLink :to="`/mobiel/${product.id}`">
+			<div :class="$style['image-container']">
+				<img src="https://placehold.co/160x240" :alt="product.model" />
+			</div>
+			<div :class="$style['body-container']">
+				<span :class="$style['manufacturer']">{{ product.manufacturer }}</span>
+				<h3 :class="$style['title']">{{ product.model }}</h3>
+			</div>
+		</NuxtLink>
+	</div>
+</template>
+
+<style module lang="scss">
+	.card {
+		position: relative;
+		border-radius: var(--border-radius-medium);
+		background-color: var(--color-kux-background);
+
+		a {
+			display: grid;
+			grid-gap: 0.5rem;
+			padding: 1rem;
+			text-decoration: none;
+		}
+	}
+
+	.title, .manufacturer {
+		margin: 0;
+		color: initial;
+	}
+
+	.title {
+		font-weight: 800;
+	}
+	.manufacturer {
+		color: var(--color-kux-text-tertiary);
+		font-weight: 300;
+	}
+
+	.image-container {
+		width: 100%;
+
+		img {
+			display: block;
+			margin: 0 auto;
+		}
+	}
+</style>
+```
--- pages/index.vue
+++ pages/index.vue
@@ -12,18 +12,37 @@
 		});
 
 
-	const numberOfProducts = computed(() => productList.value.length);
+	const sortedProducts = computed(() => productList.value.sort((a, b) => a.sort_order > b.sort_order ? 1 : -1));
+	const numberOfProducts = computed(() => sortedProducts.value.length);
 </script>
 
 <template>
 	<div class="container">
 		<h1><span style="font-weight: 300;">Kies uit maar liefst</span> {{ numberOfProducts }} telefoons</h1>
-		<form></form>
-		<ul v-if="productList" class="reset-list">
-			<li v-for="product in productList" :key="`product-id-${product.id}`">
-				<p>{{ product.manufacturer }}</p>
-				<h3>{{ product.model }}</h3>
-			</li>
-		</ul>
+		<form :class="$style['filter-bar']"></form>
+		<div v-if="productList" :class="`reset-list ${$style['product-list']}`">
+			<ProductCard v-for="product in sortedProducts" :key="product.id" :product="product" />
+		</div>
 	</div>
 </template>
+
+<style module>
+	.filter-bar {
+		margin: 1rem 0;
+		width: 100%;
+		height: 80px;
+		border-radius: var(--border-radius-medium);
+		background-color: var(--color-kux-background);
+	}
+	.product-list {
+		display: grid;
+		grid-template-columns: 1fr;
+		grid-gap: 1rem;
+	}
+
+	@media screen and (min-width: 768px) {
+		.product-list {
+			grid-template-columns: repeat(3, 1fr);
+		}
+	}
+</style>
